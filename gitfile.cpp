fecc8e3de1a854aaa532257f9e1a8fbfebd8e24d
INDRAZEET <indrajeet2025b01010763@abes.ac.in>
2025-09-22 23:44:21 +0530

    first commit

---
+++ gitfile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+int x;
+cout<<"Enter the integer = ";
+cin>>x;
+cout<<"Absolute value = ";
+if(x<0){
+    cout<<-1*x;
+}
+else{
+    cout<<x;
+}
+
+}
